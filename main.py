f50e263fb77a85abba2afda4a8f2b530972fc159
Ubuntu <ubuntu@ip-172-31-94-97.ec2.internal>
2019-08-06 23:26:50 +0000

    Modified python

---
+++ main.py
@@ -0,0 +1,40 @@
+from models import Breakfast
+import random
+
+main_dish = [
+    "Eggs Benedict",
+    "Pancakes",
+    "Waffles",
+    "French Toast",
+    "Crepes",
+    "Cereal",
+    "Breaksfast Burrito"
+    "Acai Breaksfast Bowl"
+    ]
+    
+side_dish = [
+    "Toast",
+    "Avocado Toast",
+    "Yogurt",
+    "Granola",
+    "Scones",
+    "Mixed Fruit",
+    "Sausage",
+    "Hash Browns",
+    "Bacon",
+    "Country Potatoes",
+    
+    
+    ]
+    
+drinks = [
+    "Milk",
+    "Coffee",
+    "Orange Juice",
+    "Apple Juice",
+    "Smoothie",
+    "Water",
+    "Mango Juice",
+    "Cranberry Juice",
+    "Hot Chocolate",
+    ]
