668c3094add043c8420aa308dba8c61c19147c17
Ubuntu <ubuntu@ip-172-31-94-97.ec2.internal>
2019-08-07 21:37:31 +0000

    NEW MF FILES BBY

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import webapp2
 import jinja2
 import os
-from models import menu
+from models import Breakfast
+from google.appengine.api import users
+import random
 
 
 the_jinja_env = jinja2.Environment(
@@ -10,40 +12,94 @@ the_jinja_env = jinja2.Environment(
     autoescape=True)
 
 
-main_dish = [
-    "Eggs Benedict",
-    "Pancakes",
-    "Waffles",
-    "French Toast",
-    "Crepes",
-    "Cereal",
-    "Breaksfast Burrito",
-    "Acai Breaksfast Bowl",
-    ]
+
+def get_main_dish():
+    main_dish = [
+        "Eggs Benedict",
+        "Pancakes",
+        "Waffles",
+        "French Toast",
+        "Crepes",
+        "Cereal",
+        "Breaksfast Burrito",
+        "Acai Breaksfast Bowl",
+        "Huevos Rancheros",
+        "Oat Meal",
+        "Omelet",
+        "Country-Fried Steak",
+        "Quesadillas",
+        "Breakfast Sandwhich"
+        ]
+    return (random.choice(main_dish))
     
-side_dish = [
-    "Toast",
-    "Avocado Toast",
-    "Yogurt",
-    "Granola",
-    "Scones",
-    "Mixed Fruit",
-    "Sausage",
-    "Hash Browns",
-    "Bacon",
-    "Country Potatoes",
+def get_side_dish():
+    side_dish = [
+        "Toast",
+        "Avocado Toast",
+        "Yogurt",
+        "Granola",
+        "Scones",
+        "Mixed Fruit",
+        "Sausage",
+        "Hash Browns",
+        "Bacon",
+        "Country Potatoes",
+        "Italian Sausage"
+        
+        
+        ]
+    return (random.choice(side_dish))
+    
+def get_drinks():
+    drinks = [
+        "Milk",
+        "Coffee",
+        "Orange Juice",
+        "Apple Juice",
+        "Smoothie",
+        "Water",
+        "Mango Juice",
+        "Cranberry Juice",
+        "Hot Chocolate",
+        ]
+    return (random.choice(drinks))
+    
+class HomeHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+    
+        main_dish = get_main_dish()
+        print(main_dish)
+        
+        side_dish = get_side_dish()
+        drinks = get_drinks()
+        
+        my_dictionary = {
+            'main': main_dish,
+            "sides": side_dish,
+            "drink": drinks
+            
+        }
+        
+        end_template=the_jinja_env.get_template("templates/results.html")
+        self.response.write(end_template.render(my_dictionary))
+
+
+        
+class MealsHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        welcome_template = the_jinja_env.get_template('templates/meals.html')
+        self.response.write(welcome_template.render())
+        
+class HistoryHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        welcome_template = the_jinja_env.get_template('templates/history.html')
+        self.response.write(welcome_template.render())
     
     
-    ]
+app = webapp2.WSGIApplication([
+    ('/', HomeHandler),
+    ('/meals', MealsHandler),
+    ('/history', HistoryHandler)
+    #('/allmemes', AllMemesHandler)
     
-drinks = [
-    "Milk",
-    "Coffee",
-    "Orange Juice",
-    "Apple Juice",
-    "Smoothie",
-    "Water",
-    "Mango Juice",
-    "Cranberry Juice",
-    "Hot Chocolate",
-    ]
+], debug=True)
--- models.py
+++ models.py
@@ -1,22 +1,29 @@
+# from google.appengine.ext import ndb
+
+# # class main_dish(ndb.Model):
+# #     name = ndb.StringProperty()
+# #     recipe_url = ndb.StringProperty()
+
+# # class side_dish(ndb.Model):
+# #     name = ndb.StringProperty()
+# #     recipe_url = ndb.StringProperty()
+    
+# # class drinks(ndb.Model):
+# #     name = ndb.StringProperty()
+# #     recipe_url = ndb.StringProperty()
+
+# class menu(ndb.Model):
+#     day = ndb.StringProperty()
+#     main_dish_name = ndb.StringProperty()
+#     main_dish_url = ndb.StringProperty()
+#     side_dish_name = ndb.StringProperty()
+#     side_dish_url = ndb.StringProperty()
+#     drink_name = ndb.StringProperty()
+#     drink_url = ndb.StringProperty()
 from google.appengine.ext import ndb
 
-# class main_dish(ndb.Model):
-#     name = ndb.StringProperty()
-#     recipe_url = ndb.StringProperty()
-
-# class side_dish(ndb.Model):
-#     name = ndb.StringProperty()
-#     recipe_url = ndb.StringProperty()
-    
-# class drinks(ndb.Model):
-#     name = ndb.StringProperty()
-#     recipe_url = ndb.StringProperty()
-
-class menu(ndb.Model):
-    day = ndb.StringProperty()
-    main_dish_name = ndb.StringProperty()
-    main_dish_url = ndb.StringProperty()
-    side_dish_name = ndb.StringProperty()
-    side_dish_url = ndb.StringProperty()
-    drink_name = ndb.StringProperty()
-    drink_url = ndb.StringProperty()
+class Breakfast(ndb.Model):
+    main = ndb.StringProperty(required=True)
+    side = ndb.StringProperty(required=True)
+    drink = ndb.StringProperty(required=True)
+    #img_choice = ndb.StringProperty(required=False)
